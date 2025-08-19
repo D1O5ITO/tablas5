90eaa6a8e0cdf54ea2bd3b800be076aac98a7566
D1O5ITO <pedrogabrielzea77@gmail.com>
2025-08-19 12:43:31 -0400

    inicio

--- modelo/config.php
+++ modelo/config.php
@@ -1,7 +1,15 @@
 <?php
-   //Archivo: config.php
-   define('DB_SERVER','localhost');
-   define('DB_USERNAME','root');
-   define('DB_PASSWORD','');
-   define('DB_NAME','bdusuarios_4_2024');
+// Archivo: config.php
+define('DB_SERVER', '192.168.190.128'); // IP de tu Ubuntu Server
+define('DB_USERNAME', 'zea');           // Usuario que creaste en MySQL
+define('DB_PASSWORD', 'Admin2025');     // Contraseña del usuario
+define('DB_NAME', 'bdusuarios_4_2024');         // Base de datos a usar
+
+// Crear conexión
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 ?>
